cb5a1e09c352d06cbe9df2b35073b89de32edefd
Mecken Swyter <meckenswyter@gmail.com>
2018-09-07 22:36:01 -0400

    added Initial Entries test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -64,20 +64,21 @@ $(function() {
       });
 
     });
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+    describe('Initial Entries', function() {
+
+      beforeEach(function(done) {
+        loadFeed(0, done);
+      });
+
+      it('is element', function() {
+        const feed = document.querySelector('.feed');
+        expect(feed.children.length).not.toBe(0);
+      });
+
+    });
+
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
